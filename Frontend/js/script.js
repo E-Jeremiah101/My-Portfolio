dc644aba464e1dd2a950708d23000259cce597e3
Jeremy <enofejeremiah87@gmail.com>
2025-08-07 18:49:47 +0100

    sidebar

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,19 +1,7 @@
-// const write =  'Building the web presence you deserve through quality, reliable code that’s designed to grow with your goals';
-// const target = document.getElementById("typing-text");
-// let index = 0;
-
-// function typeLetter(){
-//  if(index < text.length){
-
-//   target.textContent += text.charAt(index);
-//       index++;
-//       setTimeout(typeLetter, 100); // typing speed (ms)
-//  }
-//  typeLetter()
-// }
 const text = "Building the web presence you deserve through quality, reliable code that’s designed to grow with your goals.";
 
   const target = document.getElementById("typing-text");
+
   let index = 0;
 
   function typeLetter() {
@@ -21,7 +9,9 @@ const text = "Building the web presence you deserve through quality, reliable co
       target.textContent += text.charAt(index);
       index++;
       setTimeout(typeLetter, 100); // typing speed (ms)
+
     }
+  
   }
 
   typeLetter();
